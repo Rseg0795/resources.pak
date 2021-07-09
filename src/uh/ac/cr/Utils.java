5d78c6eaa3c4fda44bbdf3f415369f723ba6af0c
Jose David Flores <flores.jdfr@gmail.com>
2021-07-09 15:47:49 -0600

    Clase utils

---
+++ src/uh/ac/cr/Utils.java
@@ -0,0 +1,15 @@
+package uh.ac.cr;
+
+public class Utils {
+    public final static void pressEnterToContinue()
+    {
+        System.out.println("Press Enter key to continue...");
+        try
+        {
+            System.in.read();
+        }
+        catch(Exception e)
+        {}
+    }
+}
+
