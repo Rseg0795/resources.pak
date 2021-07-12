ff07bdd2474d9be2f0060750fcc45a5e88a46415
Rseg0795 <ricardo.arias@uhispano.ac.cr>
2021-07-11 19:01:50 -0600

    Update LoanManager.java

--- src/uh/ac/cr/LoanManager.java
+++ src/uh/ac/cr/LoanManager.java
@@ -1,14 +1,14 @@
 package uh.ac.cr;
 
 
- 
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class LoanManager {
 
-    private ArrayList<Loan> ArrayLoan = new ArrayList<>();
+
     ArrayList<Author> listaAutores = new ArrayList<>();
     ArrayList<Book> listaLibros = new ArrayList<>();
     ArrayList<Editorial> listaEditorial = new ArrayList<>();
@@ -22,7 +22,7 @@ public class LoanManager {
     }
 
     Scanner leer = new Scanner(System.in);
-    ArrayList <Prestamo> libros = new ArrayList <>();
+
     String [] menu = {"1 - Alta Libro", "2 - Baixa libro (buscando por ISBN)", "3 - Modificacion libro (buscando por ISBN)", "4 - Lista de libros por numero de paxinas", "5 - Lista de libros por autor", "6 - Salir"};
     String [] menuModificar = {"¿Que deseas modificar?", "1 - Título", "2 - Autor", "3 - Numero de páginas", "4 - Terminar modificación"};
     int respuesta = 0;
@@ -32,7 +32,7 @@ public class LoanManager {
     String autor;
     String titulo;
     int contadorComprobante;
-
+    private ArrayList<Loan> ArrayLoan = new ArrayList<>();
     //Iniciamos el menu
         while (respuesta != 6) {
 
@@ -57,16 +57,16 @@ public class LoanManager {
                 System.out.print("\nNúmero de páginas:  ");
                 paxinas = leer.nextInt();
 
-                libros.add(new Prestamo(isbn, titulo, autor, paxinas));
+                listaLibros.add(new ArrayLoan(isbn, titulo, autor, paxinas));
                 break;
 
             case 2:
                 System.out.print("\nIntroduce codigo ISBN a eliminar: ");
                 isbn = leer.nextInt();
                 contadorComprobante = 1;
-                for ( int contador = 0; contador < libros.size(); contador++ ) {
-                    if (isbn == libros.get(contador).getISBN()) {
-                        libros.remove(contador);
+                for ( int contador = 0; contador < listaLibros.size(); contador++ ) {
+                    if (isbn == listaLibros.get(contador).getISBN()) {
+                        listaLibros.remove(contador);
                         System.out.print("\nLibro borrado.");
                         contadorComprobante = 0;
                     }
@@ -74,7 +74,7 @@ public class LoanManager {
                         contadorComprobante++;
                     }
                 }
-                if ( contadorComprobante == libros.size() ) {
+                if ( contadorComprobante == listaLibros.size() ) {
                     System.out.print("\nNo se encuentra el código ISBN en la base de datos.");
                 }
                 break;
@@ -85,8 +85,8 @@ public class LoanManager {
                 isbn = leer.nextInt();
                 respuesta1 = 0;
                 contadorComprobante = 1;
-                for ( int contador = 0; contador < libros.size(); contador++ ) {
-                    if (isbn == libros.get(contador).getISBN()) {
+                for ( int contador = 0; contador < listaLibros.size(); contador++ ) {
+                    if (isbn == listaLibros.get(contador).getISBN()) {
                         contadorComprobante = 0;
                         while ( respuesta1 != 4 ) {
                             for (int contador1 = 0; contador1 < menuModificar.length; contador1++ ) {
@@ -99,21 +99,21 @@ public class LoanManager {
                                 case 1:
                                     System.out.print("\nIntroduce nuevo título: ");
                                     titulo = leer.nextLine();
-                                    libros.get(contador).setTitulo(titulo);
+                                    listaLibros.get(contador).setTitulo(titulo);
                                     System.out.print("\nTítulo modificado.");
                                     break;
 
                                 case 2:
                                     System.out.print("\nIntroduce nuevo autor: ");
                                     autor = leer.nextLine();
-                                    libros.get(contador).setAutor(autor);
+                                    listaLibros.get(contador).setAutor(autor);
                                     System.out.print("\nAutor modificado.");
                                     break;
 
                                 case 3:
                                     System.out.print("\nIntroduce nuevo número de páginas: ");
                                     paxinas = leer.nextInt();
-                                    libros.get(contador).setPaxinas(paxinas);
+                                    listaLibros.get(contador).setPaxinas(paxinas);
                                     System.out.print("\nTítulo modificado.");
                                     break;
                             }
@@ -123,7 +123,7 @@ public class LoanManager {
                         contadorComprobante++;
                     }
                 }
-                if ( contadorComprobante == libros.size() ) {
+                if ( contadorComprobante == listaLibros.size() ) {
                     System.out.print("\nNo se encuentra el código ISBN en la base de datos.");
                 }
                 break;
@@ -151,5 +151,5 @@ public class LoanManager {
         }
     }
 }
-}
+
 
